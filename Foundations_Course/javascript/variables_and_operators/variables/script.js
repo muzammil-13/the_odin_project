13983b4cba22b57d512bd7966d4e6766f1df4cd9
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-02-22 10:42:34 +0530

    Add HTML and JavaScript files for variables and operators lesson

---
+++ Foundations_Course/javascript/variables_and_operators/variables/script.js
@@ -0,0 +1,13 @@
+let fname="Muzammil"
+let lname="Ibrahim"
+
+console.log(fname)
+console.log(lname)
+
+let age=19
+console.log(`${fname}'s age in (july 2020): `,age)
+
+age=23.7
+console.log(`${fname}'s age in (feb 2025): `,age)
+
+const pi=3.14
