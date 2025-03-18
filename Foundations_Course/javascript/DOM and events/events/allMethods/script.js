bbaebef9b54ebe345a663c0eee1378541e5e0bd7
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-03-18 18:20:19 +0530

    Add HTML and JavaScript files for methods 1, 2, and 3 with alert functionality

---
+++ Foundations_Course/javascript/DOM and events/events/allMethods/script.js
@@ -0,0 +1,12 @@
+// method1
+function alertFunction(){
+    alert("Yay! you did it!")
+}
+
+const btn=document.querySelector("#btn");
+
+// method2
+btn.onclick=alertFunction;
+
+// method3
+btn.addEventListener("click",alertFunction);
