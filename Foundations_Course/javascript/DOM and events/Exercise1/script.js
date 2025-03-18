296bf8850b331c1e87408d3e3f1e37aa9a3b3106
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-03-18 12:06:38 +0530

    Enhance DOM manipulation exercise by adding a div with a black border and pink background, including nested elements: an h1 and a p element.

--- Foundations_Course/javascript/DOM and events/Exercise1/script.js
+++ Foundations_Course/javascript/DOM and events/Exercise1/script.js
@@ -7,6 +7,8 @@
 // 5. a <p> that says “ME TOO!”
 // Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
 
+// results:
+
 const container=document.querySelector("#container");
 // console.log(container)
 
@@ -23,3 +25,16 @@ const h3=document.createElement('h3')
 container.appendChild(h3);
 h3.textContent="I'm a blue h3!";
 h3.style.cssText="color:blue";
+
+// 3. a <div> with a black border and pink background color with the following elements inside of it:
+const div=document.createElement("div")
+container.appendChild(div)
+div.setAttribute("style",'border: 2px solid black;',"background:pink");
+// 4. another <h1> that says “I’m in a div”
+const h1=document.createElement("h1");
+div.appendChild(h1);
+h1.textContent="I'm in a div"
+// 5. a <p> that says “ME TOO!”
+const p2=document.createElement("p");
+div.appendChild(p2)
+p2.textContent="ME TOO!"
